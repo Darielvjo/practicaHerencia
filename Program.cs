f5e3698347496f1ee6552655681ea85140d3781e
Darielvjo <20230186@itla.edu.do>
2024-02-02 21:05:18 -0400

    nuevo repositorio

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿public class Persona
+{
+    public string? Nombre { get; set; }
+    public string? Apellido { get; set; }
+    public int Edad { get; set; }
+}
+
+public class MiembroDeLaComunidad : Persona
+{
+
+}
+
+public class Empleado : MiembroDeLaComunidad
+{
+
+}
+
+public class Estudiante : MiembroDeLaComunidad
+{
+
+}
+
+public class ExAlumno : MiembroDeLaComunidad
+{
+
+}
+
+public class Docente : Empleado
+{
+
+}
+
+public class Administrativo : Empleado
+{
+
+}
+
+public class Administrador : Docente
+{
+
+}
+
+public class Maestro : Docente
+{
+
+}
