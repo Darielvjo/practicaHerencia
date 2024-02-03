f254e30d9147a87e554025daf31d6d09afed53d3
Darielvjo <158626806+Darielvjo@users.noreply.github.com>
2024-02-03 00:41:19 -0400

    Add files via upload

---
+++ PracticaOOP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TareaPOO
+{
+    public class Persona
+    {
+        public string? Nombre { get; set; }
+        public string? Apellido { get; set; }
+        public int Edad { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ PracticaOOP/maestro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaOOP
+{
+    public class Maestro : Docente
+    {
+        public string? Especialidad { get; set; }
+    }
+}
